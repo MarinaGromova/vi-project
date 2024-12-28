b45c268fbe98cf1eac927c9e8b778c5f68a9c2fc
Marina Gromova <krapiva87sstu@mail.ru>
2024-12-28 16:47:26 +0400

    6.0 Auth with useForm

--- src/components/screens/auth/Auth.jsx
+++ src/components/screens/auth/Auth.jsx
@@ -1,19 +1,32 @@
-import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 import Layout from '../../layout/Layout'
+import Button from '../../ui/button/Button'
 
 function Auth() {
-	const [email, setEmail] = useState('')
+	const { register, handleSubmit, formState: { errors } } = useForm({
+		mode: 'onChange'
+	})
+
+	function onSubmit(data) {
+		console.log(data)
+	}
 
 	return (
 		<Layout heading='Sign in' bgImage="/images/auth.jpg">
 			<div className='wrapper-inner-page'>
-				<input
-					type='text'
-					placeholder='Enter email'
-					value={email}
-					onChange={e => setEmail(e.target.value)}
-				/>
-				{/* <input type='text' placeholder='Password' /> */}
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<input
+						type='text'
+						placeholder='Enter email'
+						{...register('email', {
+							required: 'Email is required'
+						})}
+					/>
+					<div style={{ color: 'red' }}>
+						{errors?.email && errors.email.message}
+					</div>
+					<Button>Let's go</Button>
+				</form>
 			</div>
 		</Layout>
 	)
