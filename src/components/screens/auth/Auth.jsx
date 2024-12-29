7eea666ec8c6aa17008679bd44df2608246e5c1d
Marina Gromova <krapiva87sstu@mail.ru>
2024-12-29 12:50:08 +0400

    7.0 Field, Loader

--- src/components/screens/auth/Auth.jsx
+++ src/components/screens/auth/Auth.jsx
@@ -1,34 +1,67 @@
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import Layout from '../../layout/Layout'
+import styles from '../../screens/auth/Auth.module.scss'
 import Button from '../../ui/button/Button'
+import Field from '../../ui/field/field'
+import Loader from '../../ui/loader/Loader'
+
+const isLoading = false
+const isLoadingAuth = false
 
 function Auth() {
+	const [type, setType] = useState('auth')
+	/*
+    TODO:
+
+		[] - Auth context
+		[] - Axios
+		[] - React Query
+
+	*/
 	const { register, handleSubmit, formState: { errors } } = useForm({
 		mode: 'onChange'
 	})
 
 	function onSubmit(data) {
+		//type
 		console.log(data)
 	}
 
 	return (
-		<Layout heading='Sign in' bgImage="/images/auth.jpg">
+		<>
+			<Layout heading='Sign in' bgImage="/images/auth.jpg" />
 			<div className='wrapper-inner-page'>
+				{(isLoading || isLoadingAuth) && <Loader />}
 				<form onSubmit={handleSubmit(onSubmit)}>
-					<input
+					<Field
+						error={errors?.email?.message}
+						name='email'
+						register={register}
+						options={{ required: 'Email is required' }}
 						type='text'
 						placeholder='Enter email'
-						{...register('email', {
-							required: 'Email is required'
-						})}
 					/>
-					<div style={{ color: 'red' }}>
-						{errors?.email && errors.email.message}
+					<Field
+						error={errors?.password?.message}
+						name='password'
+						register={register}
+						options={{ required: 'Password is required' }}
+						type='text'
+						placeholder='Enter Password'
+					/>
+					<div className={styles.wrapperButtons}>
+						<Button clickHandler={() => setType('auth')}>
+							Sign in
+						</Button>
+						<Button clickHandler={() => setType('reg')}>
+							Sign up
+						</Button>
 					</div>
-					<Button>Let's go</Button>
 				</form>
 			</div>
-		</Layout>
+		</>
+
 	)
 }
 
---
+++ src/components/ui/loader/Loader.jsx
@@ -0,0 +1,10 @@
+function Loader() {
+	return <img
+		src='public/images/three-dots.svg'
+		alt='loader'
+		draggable={false}
+		width={90}
+	/>
+}
+
+export default Loader
