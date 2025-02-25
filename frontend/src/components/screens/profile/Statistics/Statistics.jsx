3cb46659f981ab8e4591817ea735fea6127be961
Marina Gromova <krapiva87sstu@mail.ru>
2025-02-25 16:01:33 +0300

    16.0 доработка Statistics

--- frontend/src/components/screens/profile/Statistics/Statistics.jsx
+++ frontend/src/components/screens/profile/Statistics/Statistics.jsx
@@ -1,20 +1,17 @@
+import { useProfile } from '../useProfile'
 import styles from './Statistics.module.scss'
 
 function Statistics () {
+const { data } = useProfile()
+
   return (
 		<div className={styles.wrapper}>
-			<div className={styles.count}>
-				<h3 className={styles.heading}>Minutes</h3>
-				<p className={styles.color}>20</p>
-			</div>
-			<div className={styles.count}>
-				<h3 className={styles.heading}>Level</h3>
-				<p className={styles.number}>Hard</p>
-			</div>
-			<div className={styles.count}>
-				<h3 className={styles.heading}>Up</h3>
-				<p className={styles.number}>5%</p>
-			</div>			
+			{data?.statistics?.map(statistic =>
+				<div key={statistic.label} className={styles.count}>
+					<h3 className={styles.heading}>{statistic.label}</h3>
+					<p className={styles.number}>{statistic.value}</p>
+				</div>
+			)}
 		</div>
 	)
 }
