36bd898949654c8ce10f0926909377b2f4118c6c
Marina Gromova <krapiva87sstu@mail.ru>
2025-03-21 16:46:53 +0300

    18.0 SelectExercises

--- frontend/src/components/screens/new-workout/NewWorkout.jsx
+++ frontend/src/components/screens/new-workout/NewWorkout.jsx
@@ -1,15 +1,23 @@
-import cn from 'clsx'
-import { Controller } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import Layout from '../../layout/Layout'
 import Alert from '../../ui/alert/Alert'
 import Button from '../../ui/button/Button'
 import Field from '../../ui/field/field'
 import Loader from '../../ui/loader/Loader'
-import { getIconPath } from './icon-path.util'
-import styles from './NewExercise.module.scss'
+import SelectExercises from './SelectExercises'
+import { useNewWorkout } from './useNewWorkout'
 
 function NewWorkout() {
-	const { error, isSuccess, isLoading, errors, handleSubmit, onSubmit, register, control } = useExercise()
+	const { 
+		error,
+		isSuccess,
+		isLoading,
+		errors,
+		handleSubmit,
+		onSubmit,
+		register,
+		control 
+	} = useNewWorkout()
 
 	return (
 		<>
@@ -19,7 +27,7 @@ function NewWorkout() {
 			/>
 			<div className='wrapper-inner-page'>
 				{error && <Alert type='error' text={error} />}
-				{isSuccess && <Alert text='Workout created' />}
+				{isSuccess && <Alert text='Workout created successfully' />}
 				{isLoading && <Loader />}
 				<form onSubmit={handleSubmit(onSubmit)}>
 					<Field
@@ -33,41 +41,11 @@ function NewWorkout() {
 						placeholder='Enter name'
 					/>
 
-					<Field
-						error={errors?.times?.message}
-						name='times'
-						register={register}
-						options={{
-							valueAsNumber: true,
-							validate: value => value > 0 || 'Times must be number',
-							required: 'Times is required'
-						}}
-						placeholder='Enter times'
-					/>
+					<Link to='/new-exercise' className='dark-link'>
+						Add new exercise
+					</Link>
 
-					<Controller
-						name='iconPath'
-						control={control}
-						render={({ field: { value, onChange } }) => (
-							<div className={styles.images}>
-								{data.map(name => (
-									<img
-										key={`ex img ${name}`}
-										src={`${import.meta.env.VITE_SERVER_URL}${getIconPath(
-											name
-										)}`}
-										alt={name}
-										className={cn({
-											[styles.active]: value === getIconPath(name)
-										})}
-										onClick={() => onChange(getIconPath(name))}
-										draggable={false}
-										height='45'
-									/>
-								))}
-							</div>
-						)}
-					/>
+					<SelectExercises control={control} />
 
 					{errors?.iconPath && (
 						<div className='error'>{errors?.iconPath?.message}</div>
