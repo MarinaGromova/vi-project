f313580e74a5eccef6ad6ea982b317c77eb06520
Marina Gromova <krapiva87sstu@mail.ru>
2025-04-03 12:20:15 +0300

    27.0 ExerciseLogInfo, ExerciseLogTime

--- frontend/src/components/screens/exercise-log/ExerciseLog.jsx
+++ frontend/src/components/screens/exercise-log/ExerciseLog.jsx
@@ -1,40 +1,15 @@
-// import { useMutation, useQuery } from '@tanstack/react-query'
 import cn from 'clsx'
-import { useEffect, useState } from 'react'
-import { BsArrowRepeat } from "react-icons/bs"
-import { GoArrowLeft } from 'react-icons/go'
-import { IoCheckmarkDoneCircleOutline } from "react-icons/io5"
-import { useNavigate } from 'react-router-dom'
-// import ExerciseLogService from '../../../services/exercise/exercise-log.service'
 import Header from '../../layout/header/Header'
 import stylesLayout from '../../layout/Layout.module.scss'
 import styles from '../../screens/exercise-log/ExerciseLog.module.scss'
 import Loader from '../../ui/loader/Loader'
+import { ExerciseLogInfo } from './ExerciseLogInfo'
+import { ExerciseLogTime } from './ExerciseLogTime'
 import { useExerciseLog } from './useExerciseLog'
 
 const ExerciseLog = () => {
-	const navigation = useNavigate()
-
 	const {mutate, isLoading, exerciseLog, id} = useExerciseLog()
 	
-	const [color, setColor] = useState(false)
-	const [timeActive, setTimeActive] = useState(false)
-	const [seconds, setSeconds] = useState(60)
-
-	const checkColor = (color) => {
-		mutate({id, isCompleted: color})
-		setColor(!color)
-	}
-
-	useEffect(() => {
-		if(seconds>0 && timeActive) {
-			setTimeout(setSeconds, 100, seconds-1)
-		}
-		else {
-			setTimeActive(false)
-		}
-	},[ seconds, timeActive ])
-
 	return (
 		<>
 			<div className={cn(stylesLayout.wrapper, stylesLayout.otherPage)}
@@ -45,53 +20,12 @@ const ExerciseLog = () => {
 			>			
 				<Header backLink="/workouts" />
 			</div>
-			
 			<div className='wrapper-inner-page'>
 			{isLoading ? <Loader /> : (
 				<div className={styles.wrapper}>
 					<div className={styles.item}>
-						<div>
-							<div className={styles.items}>
-							  <button className={styles.repeat} onClick={() => navigation(`/workout/${exerciseLog.workoutLogId}`) } >
-									<GoArrowLeft color='fff' fontSize={30}/>
-								</button>
-								<img src={import.meta.env.VITE_SERVER_URL + exerciseLog.exercise.iconPath} alt ={exerciseLog.exercise.name} height='44' />
-								<span>{exerciseLog.exercise.name}</span>
-							</div>
-							<p>Repeat : {exerciseLog.exercise.times}</p>
-							<p>Time : {exerciseLog.exercise.times * 4} min</p>
-						</div>
-							{seconds ?
-								(
-									<>
-										<button className={styles.repeat} onClick={()=> setTimeActive(!timeActive)}>{timeActive ? 'stop' : 'start'}
-										</button>
-										<div>{seconds * exerciseLog.exercise.times * 4}</div>
-									</>
-										) : (
-											<div className={styles.icons}>											
-												{
-													exerciseLog.isCompleted === false ? 
-													<button className={styles.repeat} onClick={() => checkColor(color)}>
-
-													{  !color ?
-														<IoCheckmarkDoneCircleOutline color='fff' fontSize={50} />	: <IoCheckmarkDoneCircleOutline color='black' fontSize={50}  />	}
-
-													</button>
-													 :
-													<button className={styles.repeat} onClick={() => checkColor(color)}>
-														{  !color ? <IoCheckmarkDoneCircleOutline color='black' fontSize={50} />
-															: <IoCheckmarkDoneCircleOutline color='fff' fontSize={50} />}													
-													</button>
-												}
-
-												<button className={styles.repeat} onClick={()=> setSeconds(60)}>
-													<BsArrowRepeat color='fff' fontSize={50} />
-												</button>
-											</div>
-
-								)  
-							}
+						<ExerciseLogInfo exerciseLog={exerciseLog} />
+						<ExerciseLogTime mutate={mutate} id = {id} exerciseLog={exerciseLog} />						
 						</div>
 				</div>
 			)}
---
+++ frontend/src/components/screens/exercise-log/ExerciseLogTime.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react'
+import { BsArrowRepeat } from "react-icons/bs"
+import { IoCheckmarkDoneCircleOutline } from 'react-icons/io5'
+import styles from '../../screens/exercise-log/ExerciseLog.module.scss'
+
+export const ExerciseLogTime = ({exerciseLog, mutate, id}) => {
+	const [color, setColor] = useState(false)
+	const [seconds, setSeconds] = useState(60)
+	const [timeActive, setTimeActive] = useState(false)
+
+	const checkColor = (color) => {
+		mutate({id, isCompleted: color})
+		setColor(!color)
+	}
+
+	useEffect(() => {
+		if(seconds>0 && timeActive) {
+			setTimeout(setSeconds, 100, seconds-1)
+		}
+		else {
+			setTimeActive(false)
+		}
+	},[ seconds, timeActive ])
+
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.item}>
+				{seconds ?
+					(
+						<>
+							<button className={styles.repeat} onClick={()=> setTimeActive(!timeActive)}>{timeActive ? 'stop' : 'start'}
+							</button>
+							<div>{seconds * exerciseLog.exercise.times * 4}</div>
+						</>
+							) : (
+								<div className={styles.icons}>											
+									{
+										exerciseLog.isCompleted === false ? 
+										<button className={styles.repeat} onClick={() => checkColor(color)}>
+
+										{  !color ?
+											<IoCheckmarkDoneCircleOutline color='fff' fontSize={50} />	: <IoCheckmarkDoneCircleOutline color='black' fontSize={50}  />	}
+
+										</button>
+											:
+										<button className={styles.repeat} onClick={() => checkColor(color)}>
+											{  !color ? <IoCheckmarkDoneCircleOutline color='black' fontSize={50} />
+												: <IoCheckmarkDoneCircleOutline color='fff' fontSize={50} />}													
+										</button>
+									}
+
+									<button className={styles.repeat} onClick={()=> setSeconds(60)}>
+										<BsArrowRepeat color='fff' fontSize={50} />
+									</button>
+								</div>
+					)  
+				}
+			</div>
+		</div>
+	)							
+}
